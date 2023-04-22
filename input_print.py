09da166a5e25768c150052dbad880564c220393d
hirokirigaya <privz286@gmail.com>
2023-04-21 22:14:03 -0300

    feat(print/input): learning python

---
+++ input_print.py
@@ -0,0 +1,5 @@
+name = input('Digite seu nome: ')
+
+print('Olá, %s meus parabéns por aprender as maneiras de printar variáveis! Essa é semelhante a C' % name)
+print('Está {}, é utilizando format'.format(name))
+print(f'{name}, por fim essa é utilizando strings formatas, ou interpolação e é a mais adotada.')
--- sum.py
+++ sum.py
@@ -1,3 +1,3 @@
 a,b = 1,2
 
-print(a,b)
+print(a + b)
