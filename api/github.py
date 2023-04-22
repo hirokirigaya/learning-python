ee96159c30d1782495b44c1ae1bef0aba775706a
hirokirigaya <privz286@gmail.com>
2023-04-22 15:38:47 -0300

    feat(api): REST api github

--- api/github.py
+++ api/github.py
@@ -1,7 +1,7 @@
 import requests
 from pprint import pprint
 
-username = 'hirokirigaya'
+username = input('Enter a GitHub username: ')
 url = f"https://api.github.com/users/{username}"
 user_data = requests.get(url).json()
 
